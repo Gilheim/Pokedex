158aa7b59d219f1bc50a4568b7b3966cef06843a
Gilheim <Guy.vernon@mac.com>
2023-07-11 12:32:52 +0100

    made pokedex

--- pokedex/src/components/pokedex/index.jsx
+++ pokedex/src/components/pokedex/index.jsx
@@ -1,10 +1,18 @@
 // Pokedex component
+import Pokecard from "../pokecard"
+
 
 function Pokedex() {
     return(
         <div>
             <h1>Pokédex</h1>
-
+            <div>
+                <Pokecard id = {1}/>
+                <Pokecard id = {2}/>
+                <Pokecard id = {3}/>
+                <Pokecard id = {4}/>
+                <Pokecard id = {5}/>
+            </div>
         </div>
     )
 }
